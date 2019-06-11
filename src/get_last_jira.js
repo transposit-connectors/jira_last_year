ac7d7346b3a20f866a092fb8ec8f21262f7c3b80
Sarah Brown <sarah@transposit.com>
2019-06-11 23:32:18 +0000

    Commit from Transposit IDE

--- src/get_last_jira.js
+++ src/get_last_jira.js
@@ -1,14 +1,18 @@
 (params) => {
   const moment = require('moment');
   const lastYr = moment().subtract('1', 'y');
-  const dayAfter = moment().subtract('1', 'y').add(1, 'd');
+  const dayAfter = moment().subtract('1', 'y').add(70, 'd');
   
-  const jql = `createdDate >= "${lastYr.year()}-${lastYr.month()+1}-${lastYr.date()}" && createdDate < "${dayAfter.year()}-${dayAfter.month()+1}-${dayAfter.date()}" && resolution = Unresolved  && reporter = currentUser()`
-console.log(jql)
-  const [first,...rest] = api.run("jira.search", {jql: jql}, {limit:10});
-  if (!first) {
+  const jql = `
+	createdDate >= "${lastYr.year()}-${lastYr.month()+1}-${lastYr.date()}" && 
+	createdDate < "${dayAfter.year()}-${dayAfter.month()+1}-${dayAfter.date()}" && 
+	resolution = Unresolved  && reporter = currentUser()`;
+
+  const jira = api.run("transposit_jira.jira.search", {jql: jql}, {limit:1})[0];
+  if (!jira) {
     return;
   }
-  const { key, fields: { summary, status: { statusCategory: { name: statusName } } } } = first;
+
+  const { key, fields: { summary, status: { statusCategory: { name: statusName } } } } = jira;
   return { key, summary, statusName };
 }
--- src/mail_to.js
+++ src/mail_to.js
@@ -6,7 +6,7 @@
   
   const { key, summary, statusName } = jira;  
   const html =`<h3>EXACTLY 1 YEAR AGO YOU FILED</h3>
-        <h1><a href="https://transposit.atlassian.net/browse/${key}">${key}</a>: ${summary}</h1>
+        <h1>${key}: ${summary}</h1>
 		<hr/>
         <p style="letter-spacing: 1.4px">It's made a nice home for itself in the backlog</p>
         `
