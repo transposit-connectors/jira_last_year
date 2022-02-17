15ef3bd2d1596a3ca340d84876753004ef0e26be
iggy_lint <iggy@transposit.com>
2022-02-17 21:48:12 +0000

    iggy fixed your formatting with Prettier

--- src/get_last_jira.js
+++ src/get_last_jira.js
@@ -1,18 +1,28 @@
 (params) => {
-  const moment = require('moment');
-  const lastYr = moment().subtract('1', 'y');
-  const dayAfter = moment().subtract('1', 'y').add(1, 'd');
-  
+  const moment = require("moment");
+  const lastYr = moment().subtract("1", "y");
+  const dayAfter = moment().subtract("1", "y").add(1, "d");
+
   const jql = `
-	createdDate >= "${lastYr.year()}-${lastYr.month()+1}-${lastYr.date()}" && 
-	createdDate < "${dayAfter.year()}-${dayAfter.month()+1}-${dayAfter.date()}" && 
+	createdDate >= "${lastYr.year()}-${lastYr.month() + 1}-${lastYr.date()}" && 
+	createdDate < "${dayAfter.year()}-${
+    dayAfter.month() + 1
+  }-${dayAfter.date()}" && 
 	resolution = Unresolved  && reporter = currentUser()`;
 
-  const jira = api.run("jira.search", {jql: jql}, {limit:1})[0];
+  const jira = api.run("jira.search", { jql: jql }, { limit: 1 })[0];
   if (!jira) {
     return;
   }
 
-  const { key, fields: { summary, status: { statusCategory: { name: statusName } } } } = jira;
+  const {
+    key,
+    fields: {
+      summary,
+      status: {
+        statusCategory: { name: statusName },
+      },
+    },
+  } = jira;
   return { key, summary, statusName };
-}
+};
--- src/mail_to.js
+++ src/mail_to.js
@@ -3,17 +3,17 @@
   if (!jira) {
     return;
   }
-  
-  const { key, summary, statusName } = jira;  
-  const html =`<h3>EXACTLY 1 YEAR AGO YOU FILED</h3>
+
+  const { key, summary, statusName } = jira;
+  const html = `<h3>EXACTLY 1 YEAR AGO YOU FILED</h3>
         <h1>${key}: ${summary}</h1>
 		<hr/>
         <p style="letter-spacing: 1.4px">It's made a nice home for itself in the backlog</p>
-        `
-  
+        `;
+
   return api.run("this.send_message", {
     to: api.user().email,
     subject: `Exactly 1 year ago, you filed ${key}`,
-    messageHtml: html
+    messageHtml: html,
   });
-}
+};
